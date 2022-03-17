ac0b56c2dff54ff7cd5f3ee6e5553adbb9659d3d
Abhishek Bedi <kbedi@mafcarrefour.com>
2022-03-17 23:02:24 +0400

    Initial Commit

---
+++ NYT/NYTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTApp.swift
+//  NYT
+//
+//  Created by kbedi on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NYTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
