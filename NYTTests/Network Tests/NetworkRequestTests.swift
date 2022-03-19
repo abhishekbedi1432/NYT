3701b3af5155015f514babbf3174a09506a11446
Abhishek Bedi <kbedi@mafcarrefour.com>
2022-03-19 17:33:45 +0400

    Added network request tests

--- NYTTests/Network Tests/NetworkRequestTests.swift
+++ NYTTests/Network Tests/NetworkRequestTests.swift
@@ -8,30 +8,18 @@
 import XCTest
 @testable import NYT
 
-
 class NetworkRequestTests: XCTestCase {
-
-    var request: ArticleListRequest?
-    
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        request = nil
-    }
 
     func test_requestUrl() throws {
         
         // Given
         let baseURL = "https://dummy.com"
-        request = ArticleListRequest(baseUrl: baseURL, timePeriod: 5, apiKey: "dummyKey")
+        let request = ArticleListRequest(baseUrl: baseURL, timePeriod: 5, apiKey: "dummyKey")
         
         let expectedUrlString = "https://dummy.com/v2/mostviewed/all-sections/5.json?api-key=dummyKey"
         
         // When
-        let absoluteString = try? XCTUnwrap(request?.urlRequest?.url?.absoluteString)
+        let absoluteString = try? XCTUnwrap(request.urlRequest?.url?.absoluteString)
         
         // Then
         XCTAssertEqual(absoluteString, expectedUrlString)
@@ -43,9 +31,22 @@ class NetworkRequestTests: XCTestCase {
         let baseURL = ""
         
         // When
-        request = ArticleListRequest(baseUrl: baseURL, timePeriod: 5, apiKey: "dummyKey")
+        let request = ArticleListRequest(baseUrl: baseURL, timePeriod: 5, apiKey: "dummyKey")
         
         // Then
-        XCTAssertNil(request?.urlRequest)
+        XCTAssertNil(request.urlRequest)
+    }
+    
+    
+    func test_requestUrl_without_params() throws {
+        
+        // Given
+        let baseURL = URL(string: "www.abc.com")!
+        
+        // When
+        let request = URLRequest(baseURL: baseURL, path: nil, parameters: nil)
+        
+        // Then
+        XCTAssertEqual(request?.url, baseURL)
     }
 }
