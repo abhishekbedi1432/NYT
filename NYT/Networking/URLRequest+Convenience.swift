76beb9b66429f158ab7fe9987408710685621d19
Abhishek Bedi <kbedi@mafcarrefour.com>
2022-03-19 17:53:10 +0400

    Fixed error in URLRequest

--- NYT/Networking/URLRequest+Convenience.swift
+++ NYT/Networking/URLRequest+Convenience.swift
@@ -10,8 +10,13 @@ import Foundation
 extension URLRequest {
     
     init?(baseURL: URL, path: String?, parameters: [String: AnyObject]?) {
-        let URL = baseURL.appendingPathComponent(path ?? "")
-        var components = URLComponents(url: URL, resolvingAgainstBaseURL: false)
+        
+        var url: URL = baseURL
+        if let path = path, !path.isEmpty {
+            url = baseURL.appendingPathComponent(path)
+        }
+        
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
         
         components?.queryItems = parameters?.map { key, value -> URLQueryItem in
             URLQueryItem(name: String(key), value: String(describing: value))
